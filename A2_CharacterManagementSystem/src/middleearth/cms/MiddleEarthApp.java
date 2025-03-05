e39ef7c53118fda5c20beb678f29a26d9346a74c
G-Ninja05 <108912992+G-Ninja05@users.noreply.github.com>
2025-03-04 20:57:01 -0600

    Started the council and the main classes. Still need to debug them.

--- A2_CharacterManagementSystem/src/middleearth/cms/MiddleEarthApp.java
+++ A2_CharacterManagementSystem/src/middleearth/cms/MiddleEarthApp.java
@@ -1,5 +1,8 @@
 package middleearth.cms;
 
 public class MiddleEarthApp {
-
+	public static void main(String[] args) {
+		MiddleEarthCouncil middleEarthCouncil = new MiddleEarthCouncil("Gandalf");
+		
+	}
 }
--- A2_CharacterManagementSystem/src/middleearth/cms/MiddleEarthCouncil.java
+++ A2_CharacterManagementSystem/src/middleearth/cms/MiddleEarthCouncil.java
@@ -1,5 +1,32 @@
 package middleearth.cms;
 
 public class MiddleEarthCouncil {
-
+	
+	private static MiddleEarthCouncil instance1;
+	String character;
+	
+	MiddleEarthCouncil (String character){
+		this.character = character;
+		}
+	
+	public static MiddleEarthCouncil getinstance(String character) {
+		if(instance1 == null) {
+			instance1 = new MiddleEarthCouncil(character);
+		}
+			return instance1;
+}
+	CharacterManager getCharacterManager(){
+		private static MiddleEarthCouncil instance;
+		String manager;
+		
+		private MiddleEarthCouncil instance2(String manager){
+			this.manager = manager;
+			}
+		public static MiddleEarthCouncil getinstance1(String manager) {
+			if(instance1 == null) {
+				instance1 = new MiddleEarthCouncil(manager);
+				}
+			return instance1;
+	}
+		
 }
