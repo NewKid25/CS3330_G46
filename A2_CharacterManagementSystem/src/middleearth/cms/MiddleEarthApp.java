881dc6f98a6d2fe9ec8f18dc5aa84c19dfdad9a1
G-Ninja05 <108912992+G-Ninja05@users.noreply.github.com>
2025-03-05 22:25:26 -0600

    added menu to main function. Needs to be debugged

--- A2_CharacterManagementSystem/src/middleearth/cms/MiddleEarthApp.java
+++ A2_CharacterManagementSystem/src/middleearth/cms/MiddleEarthApp.java
@@ -1,5 +1,7 @@
 package middleearth.cms;
 
+import java.util.Scanner;
+
 import middleearth.cms.MiddleEarthCouncil;
 import middleearth.cms.characters.MiddleEarthCharacter;
 import middleearth.cms.characters.Wizard;
@@ -7,16 +9,59 @@ import middleearth.cms.characters.Wizard;
 
 public class MiddleEarthApp {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		while(true){
 		MiddleEarthCouncil council = new MiddleEarthCouncil();
-		MiddleEarthCharacter character = new Wizard("Gandalf", 100.0, 5.0);
-		council.getmanagerinstance().addCharacter(character);
-		MiddleEarthCharacter character2 = new Wizard("Sauron", 100, 5);
-		council.getmanagerinstance().addCharacter(character2);
-		council.getmanagerinstance().displayAllCharacters();
-		council.getmanagerinstance().updateCharacter(character, "Gandalf", 90, 5);
-		council.getmanagerinstance().getCharacter("Gandalf").attack(character2);
-		council.getmanagerinstance().deleteCharacter(character);
-		council.getmanagerinstance().deleteCharacter(character2);
 		
+		System.out.println("Name of character: ");
+		String heroname = scanner.nextLine();
+		MiddleEarthCharacter character = new Wizard(heroname, 100.0, 5.0);
+		council.getmanagerinstance().addCharacter(character);
+		
+		System.out.println("Name of villian: ");
+		String villianname = scanner.nextLine();
+		MiddleEarthCharacter character2 = new Wizard(villianname, 100, 5);
+		council.getmanagerinstance().addCharacter(character2);
+		
+		council.getmanagerinstance().displayAllCharacters();
+		
+		System.out.println("Would you like to update your character? (y/n): ");
+		if(scanner.nextLine() == "y") {
+			System.out.println("What would you like to change? (name, hp, power)");
+			if(scanner.nextLine() == "name") {
+				System.out.println("New hero name: ");
+				String newname = scanner.nextLine();
+				council.getmanagerinstance().updateCharacter(character, newname, character.getHealth(), character.getPower());
+			}
+			if(scanner.nextLine() == "hp") {
+				System.out.println("New hero hp: ");
+				String hp = scanner.nextLine();
+				council.getmanagerinstance().updateCharacter(character, character.getName(), hp, character.getPower());
+			}
+			if(scanner.nextLine() == "power") {
+				System.out.println("New hero power: ");
+				String power = scanner.nextLine();
+				council.getmanagerinstance().updateCharacter(character, character.getName(),character.getHealth(), power);
+			}
+			else {
+				break;
+			}
+			
+		}
+		System.out.println("Who would you like to attack?: ");
+		String villian = scanner.nextLine();
+		if(council.getmanagerinstance().getCharacter(villian) != null) {
+			council.getmanagerinstance().getCharacter(heroname).attack(council.getmanagerinstance().getCharacter(villian));
+		}
+		System.out.println("Who would you like to delete your characters?: ");
+		if(scanner.nextLine() == "y") {
+			council.getmanagerinstance().deleteCharacter(character);
+			council.getmanagerinstance().deleteCharacter(character2);
+		}
+		System.out.println("Would you like to exit this program?: ");
+		if(scanner.nextLine() == "n") {
+			break;
+		}
 	}
+}
 }
