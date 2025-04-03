7cfb2901ee346259746b8f55afb38825cbc0f5fc
NewKid25 <115599059+NewKid25@users.noreply.github.com>
2025-04-02 20:21:21 -0500

    feat: MidiEventData getters/setters

--- A3_LittleMozart/src/midi/composition/data/MidiCsvParser.java
+++ A3_LittleMozart/src/midi/composition/data/MidiCsvParser.java
@@ -1,5 +1,3 @@
 package midi.composition.data;
 
-public class MidiCsvParser {
 
-}
--- A3_LittleMozart/src/midi/composition/data/MidiEventData.java
+++ A3_LittleMozart/src/midi/composition/data/MidiEventData.java
@@ -14,5 +14,53 @@ public class MidiEventData {
         this.instrument = instrument;
         this.noteOnOff = noteOnOff;
     }
-    // implement the getter / setters
+
+	public int getStartEndTick() {
+		return startEndTick;
+	}
+
+	public void setStartEndTick(int startEndTick) {
+		this.startEndTick = startEndTick;
+	}
+
+	public int getVelocity() {
+		return velocity;
+	}
+
+	public void setVelocity(int velocity) {
+		this.velocity = velocity;
+	}
+
+	public int getNote() {
+		return note;
+	}
+
+	public void setNote(int note) {
+		this.note = note;
+	}
+
+	public int getChannel() {
+		return channel;
+	}
+
+	public void setChannel(int channel) {
+		this.channel = channel;
+	}
+
+	public int getNoteOnOff() {
+		return noteOnOff;
+	}
+
+	public void setNoteOnOff(int noteOnOff) {
+		this.noteOnOff = noteOnOff;
+	}
+
+	public int getInstrument() {
+		return instrument;
+	}
+
+	public void setInstrument(int instrument) {
+		this.instrument = instrument;
+	}
+    
 }
