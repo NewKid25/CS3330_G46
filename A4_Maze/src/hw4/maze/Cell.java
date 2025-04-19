608bad80a252977e3793ef48ce44dde85539167f
NewKid25 <115599059+NewKid25@users.noreply.github.com>
2025-04-19 12:26:22 -0500

    feat: cell
    - passes all tests in CellTest.java

---
+++ A4_Maze/src/hw4/maze/Cell.java
@@ -0,0 +1,87 @@
+package hw4.maze;
+
+public class Cell {
+
+	private CellComponents left;
+	private CellComponents right;
+	private CellComponents up;
+	private CellComponents down;
+	
+
+	public Cell(CellComponents left, CellComponents right, CellComponents up, CellComponents down) {
+		super();
+		this.left = left;
+		this.right = right;
+		this.up = up;
+		this.down = down;
+	}
+	
+	@Override
+	public String toString() {
+		return "Cell [left=" + left + ", right=" + right + ", up=" + up + ", down=" + down + "]";
+	}
+
+	public CellComponents getLeft() {
+		return left;
+	}
+
+	public void setLeft(CellComponents left) {
+		if(left == null)
+		{
+			this.left = CellComponents.WALL;
+		}
+		else
+		{			
+			this.left = left;
+		}
+	}
+
+	public CellComponents getRight() {
+		return right;
+	}
+
+	public void setRight(CellComponents right) {
+		if(right == null)
+		{
+			this.right = CellComponents.WALL;
+		}
+		else
+		{			
+			this.right = right;
+		}
+	}
+
+	public CellComponents getUp() {
+		return up;
+	}
+
+	public void setUp(CellComponents up) {
+		if(up == null)
+		{
+			this.up = CellComponents.WALL;
+		}
+		else
+		{			
+			this.up = up;
+		}
+	}
+
+	public CellComponents getDown() {
+		return down;
+	}
+
+	public void setDown(CellComponents down) {
+		if(down == null)
+		{
+			this.down = CellComponents.WALL;
+		}
+		else
+		{			
+			this.down = down;
+		}
+	}
+	
+
+	
+
+}
