98c72555d8a097e05990010aa0fd57c7e6358094
G-Ninja05 <gjamesj14@gmail.com>
2025-05-02 18:57:54 -0500

    finished and fixed window (needs debugging)

--- A5_PetCenter/src/main/java/views/PetDetailsView.java
+++ A5_PetCenter/src/main/java/views/PetDetailsView.java
@@ -6,9 +6,18 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import models.NormalAnimal;
+import models.Pet;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class PetDetailsView extends JFrame {
 	private JPanel panel;
+	public JTextField petName;
+	public JTextField petID;
+	public JTextField petType;
+	public JTextField petSpecies;
+	public JTextField petAge;
 
 	public PetDetailsView() throws HeadlessException {
 		setTitle("Animal Details Page");
@@ -19,16 +28,60 @@ public class PetDetailsView extends JFrame {
 		setContentPane(panel);
 		getContentPane().setLayout(null);
 		
+		JLabel nameLabel = new JLabel("Name");
+		nameLabel.setBounds(94, 40, 61, 16);
+		panel.add(nameLabel);
+		
+		JLabel idLabel = new JLabel("ID:");
+		idLabel.setBounds(94, 81, 61, 16);
+		panel.add(idLabel);
+		
+		JLabel typeLabel = new JLabel("Type:");
+		typeLabel.setBounds(94, 117, 61, 16);
+		panel.add(typeLabel);
+		
+		JLabel speciesLabel = new JLabel("Species:");
+		speciesLabel.setBounds(94, 158, 61, 16);
+		panel.add(speciesLabel);
+		
+		JLabel ageLabel = new JLabel("Age:");
+		ageLabel.setBounds(94, 200, 61, 16);
+		panel.add(ageLabel);
+		
+		petName = new JTextField();
+		petName.setEditable(false);
+		petName.setBounds(167, 35, 130, 26);
+		panel.add(petName);
+		petName.setColumns(10);
+		
+		petID = new JTextField();
+		petID.setEditable(false);
+		petID.setColumns(10);
+		petID.setBounds(167, 76, 130, 26);
+		panel.add(petID);
+		
+		petType = new JTextField();
+		petType.setEditable(false);
+		petType.setColumns(10);
+		petType.setBounds(167, 112, 130, 26);
+		panel.add(petType);
+		
+		petSpecies = new JTextField();
+		petSpecies.setEditable(false);
+		petSpecies.setColumns(10);
+		petSpecies.setBounds(167, 153, 130, 26);
+		panel.add(petSpecies);
+		
+		petAge = new JTextField();
+		petAge.setEditable(false);
+		petAge.setColumns(10);
+		petAge.setBounds(167, 195, 130, 26);
+		panel.add(petAge);
+		
+		this.setVisible(false);
 	}
-	public int getPetDetails() {
-//		PetView petview = new PetView();
-//		int index = petview.adoptList.getSelectedIndex();
-//		petview.adoptList.getSelectedValue().getClass().getName();
-//		petview.adoptList.getSelectedValue().getClass().getId();
-//		petview.adoptList.getSelectedValue().getClass().gettype();
-//		petview.adoptList.getSelectedValue().getClass().getspecies();
-//		petview.adoptList.getSelectedValue().getClass().getage();
-		return 0;
+	public void initiate() {
+		this.setVisible(true);
+		
 	}
-	
 }
